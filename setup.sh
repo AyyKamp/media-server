acc26c1178cda671ccafe0690962308faef9b298
AyyKamp <arda513@hotmail.de>
2020-07-03 15:06:55 +0200

    setup.sh

--- setup.sh
+++ setup.sh
@@ -1,1 +1,6 @@
-useradd -m -p "1234" "user"
+useradd -m -p "$1" "user"
+cp -r /root/ /home/user/
+chsh -s /bin/bash user
+
+## TODO add plex claim option and mount gdrive (optimally give url) (use rclone or google-drive-ocamlfuse)
+/bin/su -c "/path/to/start_docker.sh argument" - user
